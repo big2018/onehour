f911a158ea62d32894443875a0613c7416016837
james2008 <564088904@qq.com>
2018-06-05 20:49:36 +0800

    add MyApp

---
+++ app/src/main/java/com/example/quarterhour/app/MyApp.java
@@ -0,0 +1,14 @@
+package com.example.quarterhour.app;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+public class MyApp extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(this);
+    }
+}
