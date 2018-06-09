1b0f2c50e04b42d033a93edd2e9c97d22a4ebd43
james2008 <564088904@qq.com>
2018-06-09 17:28:26 +0800

    add share

--- app/src/main/java/com/example/quarterhour/app/MyApp.java
+++ app/src/main/java/com/example/quarterhour/app/MyApp.java
@@ -9,6 +9,8 @@ import com.umeng.socialize.PlatformConfig;
 public class MyApp extends Application{
     {
         PlatformConfig.setQQZone("1106791541", "4cjDBYUvtHJlkML0");
+        PlatformConfig.setWeixin("wxc7309f423b8a210c","b3e8a1b42b14f983409caee8e8125790");
+
     }
 
     @Override
@@ -19,5 +21,7 @@ public class MyApp extends Application{
         UMConfigure.init(this,"5b19391df43e4835440000c6"
                 ,"umeng",UMConfigure.DEVICE_TYPE_PHONE,"");//58edcfeb310c93091c000be2 5965ee00734be40b580001a0
 
+//        UMConfigure.setLogEnabled(true);
+//        MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
     }
 }
--- app/src/main/java/com/example/quarterhour/ui/details/VideoDetailsActivity.java
+++ app/src/main/java/com/example/quarterhour/ui/details/VideoDetailsActivity.java
@@ -6,11 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.quarterhour.MainActivity;
 import com.example.quarterhour.R;
 import com.example.quarterhour.bean.VideosBean;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.editorpage.ShareActivity;
+import com.umeng.socialize.media.UMVideo;
+import com.umeng.socialize.media.UMWeb;
 
 import cn.jzvd.JZVideoPlayerStandard;
 
@@ -26,6 +35,7 @@ public class VideoDetailsActivity extends AppCompatActivity implements View.OnCl
     private TextView mTvTime;
     private boolean xhbool = true;
     private boolean tybool = true;
+    private VideosBean.DataBean dataBean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +44,14 @@ public class VideoDetailsActivity extends AppCompatActivity implements View.OnCl
         initView();
 
         Intent intent = getIntent();
-        VideosBean.DataBean dataBean = (VideosBean.DataBean) intent.getSerializableExtra("videosbean");
+        dataBean = (VideosBean.DataBean) intent.getSerializableExtra("videosbean");
 
         mSimpleTx.setImageURI(dataBean.getUser().getIcon());
-        mPlayer.setUp(dataBean.getVideoUrl(),JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL,dataBean.getWorkDesc());
+        mPlayer.setUp(dataBean.getVideoUrl(),JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, dataBean.getWorkDesc()+"");
         Glide.with(this).load(dataBean.getVideoUrl()).into(mPlayer.thumbImageView);
 
         mTvJs.setText(dataBean.getWorkDesc());
-        mTvTime.setText(dataBean.getPlayNum()+"次播放    "+dataBean.getCreateTime());
+        mTvTime.setText(dataBean.getPlayNum()+"次播放    "+ dataBean.getCreateTime());
 
     }
 
@@ -72,23 +82,33 @@ public class VideoDetailsActivity extends AppCompatActivity implements View.OnCl
                 break;
 
             case R.id.img_xh:
-                if (xhbool){
-                    mImgXh.setImageResource(R.drawable.raw1499933215);
-                    xhbool = !xhbool;
+                if (tybool){
+                    if (xhbool){
+                        mImgXh.setImageResource(R.drawable.raw1499933215);
+                        xhbool = !xhbool;
+                    }else {
+                        mImgXh.setImageResource(R.drawable.raw_1499933216);
+                        xhbool = !xhbool;
+                    }
                 }else {
-                    mImgXh.setImageResource(R.drawable.raw_1499933216);
-                    xhbool = !xhbool;
+
                 }
+
                 break;
 
             case R.id.img_ty:
-                if (tybool){
-                    mImgTy.setImageResource(R.drawable.raw499933216);
-                    tybool = !tybool;
+                if(xhbool){
+                    if (tybool){
+                        mImgTy.setImageResource(R.drawable.raw499933216);
+                        tybool = !tybool;
+                    }else {
+                        mImgTy.setImageResource(R.drawable.raw_1499933217);
+                        tybool = !tybool;
+                    }
                 }else {
-                    mImgTy.setImageResource(R.drawable.raw_1499933217);
-                    tybool = !tybool;
+
                 }
+
                 break;
 
             case R.id.simple_tx:
@@ -97,7 +117,67 @@ public class VideoDetailsActivity extends AppCompatActivity implements View.OnCl
                 break;
 
             case R.id.img_fx:
+//                UMWeb web = new UMWeb(dataBean.getVideoUrl());,SHARE_MEDIA.WEIXIN
+                UMVideo video = new UMVideo(dataBean.getVideoUrl());
+                new ShareAction(VideoDetailsActivity.this).withMedia(video).setDisplayList(SHARE_MEDIA.QQ,SHARE_MEDIA.WEIXIN,SHARE_MEDIA.QZONE,SHARE_MEDIA.WEIXIN_CIRCLE)
+                        .setCallback(shareListener).open();
+//                new ShareAction(VideoDetailsActivity.this)
+//                        .setPlatform(SHARE_MEDIA.QQ)//传入平台
+//                        .withText("hello")//分享内容
+//                        .withMedia(video)
+//                        .setCallback(shareListener)//回调监听器
+//                        .share();
+//                new ShareAction(VideoDetailsActivity.this).withText("hello").withMedia(video).share();
                 break;
         }
     }
+
+    private UMShareListener shareListener = new UMShareListener() {
+        /**
+         * @descrption 分享开始的回调
+         * @param platform 平台类型
+         */
+        @Override
+        public void onStart(SHARE_MEDIA platform) {
+
+        }
+
+        /**
+         * @descrption 分享成功的回调
+         * @param platform 平台类型
+         */
+        @Override
+        public void onResult(SHARE_MEDIA platform) {
+            Toast.makeText(VideoDetailsActivity.this,"成功了",Toast.LENGTH_LONG).show();
+        }
+
+        /**
+         * @descrption 分享失败的回调
+         * @param platform 平台类型
+         * @param t 错误原因
+         */
+        @Override
+        public void onError(SHARE_MEDIA platform, Throwable t) {
+            Toast.makeText(VideoDetailsActivity.this,"失败"+t.getMessage(),Toast.LENGTH_LONG).show();
+        }
+
+        /**
+         * @descrption 分享取消的回调
+         * @param platform 平台类型
+         */
+        @Override
+        public void onCancel(SHARE_MEDIA platform) {
+            Toast.makeText(VideoDetailsActivity.this,"取消了",Toast.LENGTH_LONG).show();
+
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
 }
